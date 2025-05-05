f76060915c98f94ed59c1dd54893caddd6e11d6a
Fabian Ferno <fabianferno@gmail.com>
2025-05-05 16:31:37 +0530

    Revert "Merge branch 'main' of https://github.com/fabianferno/luksocal"
    This reverts commit 5b7f5c4f991372eee6bf890ac32030ff47588611, reversing
    changes made to 704a4c829d1b43f0bf99232c7994478a32c2bcd5.

--- miniapp/app/layout.tsx
+++ miniapp/app/layout.tsx
@@ -1,6 +1,6 @@
-import Provider from "@/lib/Provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import '../styles/globals.scss';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Provider>{children}</Provider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
